604b6cf353ba77fa7f03df58d20ba3739161ffa4
Yasin Küyük <yasinkuyuk@gmail.com>
2023-04-21 23:36:31 +0300

    Added logo and favicon

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="bg-slate-100 min-h-screen text-center">
-    <div class="px-3 md:px-10 pt-4">
+    <div class="px-3 md:px-10">
       <div class="grid grid-cols-3">
         <div class="col-span-3 md:col-span-1 justify-end md:pr-8 md:pt-10">
+          <img src="logo.png" class="h-36 mx-auto" />
+          <div class="mt-2">
+            <flip-countdown :deadline="deadline"></flip-countdown>
+          </div>
           <div
             class="px-4 mb-4 py-2 text-left mt-2 bg-amber-50 rounded-md border-l-4 border-l-yellow-600 shadow-lg text-xs"
           >
@@ -59,19 +63,9 @@
             seçim verilerinin korunabilmesi adına
             <span class="italic">Oy Oranları</span> bölümünde yer almaktadır.
           </div>
-          <div class="mt-12">
-            <flip-countdown
-              :deadline="deadline"
-              :locale="locale"
-            ></flip-countdown>
-          </div>
         </div>
-        <div class="col-span-3 md:col-span-2">
-          <h2 class="text-2xl font-semibold text-gray-400">
-            Seçim 2023 - D'Hondt Simülatörü
-          </h2>
+        <div class="col-span-3 md:col-span-2 my-auto">
           <h3 class="text-xl font-bold">{{ currentProvince.name }}</h3>
-
           <turkey-map></turkey-map>
         </div>
       </div>
